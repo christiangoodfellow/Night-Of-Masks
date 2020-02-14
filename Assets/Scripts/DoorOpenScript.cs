d5e643d4b037509a4c1d27bd587c32f2045d9fe2
Lawrence Simons <lawrencejohnsimons@gmail.com>
2020-02-14 10:38:12 -0500

    I made a door
    I made a door that opens and closes based on it's boolean.

---
+++ Assets/Scripts/DoorOpenScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenScript : MonoBehaviour
+{
+
+    public bool isOpen;
+    public float openSpeed;
+    public Transform doorRotatePoint;
+    private Quaternion closedQuaternion;
+    public Transform doorOpenRotatePoint;
+    private Quaternion openQuaternion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        closedQuaternion = doorRotatePoint.rotation;
+        openQuaternion = doorOpenRotatePoint.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isOpen)
+        {
+            if (Mathf.Abs(doorRotatePoint.rotation.y - openQuaternion.y) > .01)
+            { 
+                doorRotatePoint.rotation = Quaternion.Slerp(doorRotatePoint.rotation, openQuaternion, openSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if(Mathf.Abs(doorRotatePoint.rotation.y - closedQuaternion.y) > .01)
+            {
+                doorRotatePoint.rotation = Quaternion.Slerp(doorRotatePoint.rotation, closedQuaternion, openSpeed * Time.deltaTime);
+            }
+        }
+        
+
+    }
+}
